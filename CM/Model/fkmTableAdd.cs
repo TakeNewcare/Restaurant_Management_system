86b9e57d667059da8b2d861bbb318e81e5c117aa
TakeNewcare <163362484+TakeNewcare@users.noreply.github.com>
2024-07-02 22:13:27 +0900

    Add files via upload

---
+++ CM/Model/fkmTableAdd.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CM.Model
+{
+    public partial class fkmTableAdd : SampleAdd
+    {
+        public fkmTableAdd()
+        {
+            InitializeComponent();
+        }
+
+        public int id = 0;  //view 에서 설정해준다.
+
+        // 저장, 수정 버튼 
+        public override void btnSave_Click(object sender, EventArgs e)
+        {
+            // 화면 출력이 아니라 쿼리만 실행하기 때문에 sql 함수 사용
+            // 데이터 저장용, 수정용 쿼리 
+            string qry = "";
+            if (id == 0)    // 데이터 저장용
+            {
+                qry = "insert into tables values(@Name)";
+            }
+            else
+            {
+                qry = "update tables set tName = @Name where tID = @id";
+            }
+
+            Hashtable ht = new Hashtable();     // 그리드뷰의 데이터를 헤쉬테이블의 키-값 형태로 만들기
+            ht.Add("@id", id);
+            ht.Add("@Name", txtName1.Text);
+
+            if (MainClass.SQL(qry, ht) > 0)  // 실행한 결과의 값
+            {
+                guna2MessageDialog1.Show("저장 완료");
+                id = 0;                     // => 만약, 수정했을 때 바로 작성하면 추가 기능으로 변한다.
+                txtName1.Text = "";
+                txtName1.Focus();
+            }
+
+
+        }
+
+    }
+}
---
+++ CM/Model/urProduct.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CM.Model
+{
+    partial class urProduct
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            this.guna2Separator1 = new Guna.UI2.WinForms.Guna2Separator();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtImage = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2ShadowPanel1.SuspendLayout();
+            this.guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2ShadowPanel1
+            // 
+            this.guna2ShadowPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ShadowPanel1.Controls.Add(this.guna2Separator1);
+            this.guna2ShadowPanel1.Controls.Add(this.guna2Panel1);
+            this.guna2ShadowPanel1.Controls.Add(this.txtImage);
+            this.guna2ShadowPanel1.FillColor = System.Drawing.Color.White;
+            this.guna2ShadowPanel1.Location = new System.Drawing.Point(3, 3);
+            this.guna2ShadowPanel1.Name = "guna2ShadowPanel1";
+            this.guna2ShadowPanel1.ShadowColor = System.Drawing.Color.Black;
+            this.guna2ShadowPanel1.Size = new System.Drawing.Size(169, 198);
+            this.guna2ShadowPanel1.TabIndex = 0;
+            // 
+            // guna2Separator1
+            // 
+            this.guna2Separator1.Location = new System.Drawing.Point(4, 130);
+            this.guna2Separator1.Name = "guna2Separator1";
+            this.guna2Separator1.Size = new System.Drawing.Size(162, 10);
+            this.guna2Separator1.TabIndex = 2;
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Controls.Add(this.lblName);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 143);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(169, 55);
+            this.guna2Panel1.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(13, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(143, 55);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "fsdfasf";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtImage
+            // 
+            this.txtImage.Image = global::CM.Properties.Resources.briyani;
+            this.txtImage.ImageRotate = 0F;
+            this.txtImage.Location = new System.Drawing.Point(33, 18);
+            this.txtImage.Name = "txtImage";
+            this.txtImage.Size = new System.Drawing.Size(101, 102);
+            this.txtImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.txtImage.TabIndex = 0;
+            this.txtImage.TabStop = false;
+            this.txtImage.Click += new System.EventHandler(this.txtImage_Click);
+            // 
+            // urProduct
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.guna2ShadowPanel1);
+            this.Name = "urProduct";
+            this.Size = new System.Drawing.Size(175, 204);
+            this.guna2ShadowPanel1.ResumeLayout(false);
+            this.guna2Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.txtImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
+        private Guna.UI2.WinForms.Guna2PictureBox txtImage;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private System.Windows.Forms.Label lblName;
+        private Guna.UI2.WinForms.Guna2Separator guna2Separator1;
+    }
+}
