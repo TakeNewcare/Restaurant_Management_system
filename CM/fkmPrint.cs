310e222854fbf2443efa4e043f1fcdacb7e549ed
TakeNewcare <163362484+TakeNewcare@users.noreply.github.com>
2024-07-02 22:33:50 +0900

    Add files via upload

---
+++ CM/fkmPrint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CM
+{
+    public partial class fkmPrint : Form
+    {
+        public fkmPrint()
+        {
+            InitializeComponent();
+        }
+
+        private void fkmPrint_Load(object sender, EventArgs e)
+        {
+            btnMax.PerformClick();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
